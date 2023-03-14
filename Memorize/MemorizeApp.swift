daff7b21b7c17b8b74b574331f6b2dc913bb722b
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-03-14 17:41:44 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gabriel on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
