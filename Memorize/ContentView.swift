ebb014e5a5d5c184cc96d648aa7edfed87d35288
Gabriel Cordeiro <gabrielcemp@gmail.com>
2023-03-16 20:17:01 -0300

    add emoji memory game view model

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var game: EmojiMemoryGame
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(game.cards) { card in
+                CardView(card: card).onTapGesture {
+                    game.choose(card: card)
+                }
+            }
         }
-        .padding()
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
 
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(game: EmojiMemoryGame())
     }
 }
---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Gabriel on 15/03/23.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var game: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        game.cards
+    }
+    
+    func choose(card: MemoryGame<String>.Card) {
+        game.choose(card: card)
+    }
+}
